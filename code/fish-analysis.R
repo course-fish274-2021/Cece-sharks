f3b31a17a52b2b66c34ef5da44cedbd9a4f19e5a
Cece <93742814+cwendel23@users.noreply.github.com>
2021-11-16 10:16:34 -0800

    Removed fish from Gaeta data with scale length < 1

--- code/fish-analysis.R
+++ code/fish-analysis.R
@@ -3,6 +3,10 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data_1.csv")
 library(dplyr)
 library(ggplot2)
 
-# Adding column to denote if fish is big or small, using 300 as a cutoff
+# Adding column to denote if fish is big or small, using 300 as a cutoff (command: mutate)
+# Removed any fish with a scalelength less than 1mm from data (command: filter)
 fish_data_cat <- fish_data %>%
+  filter(scalelength > 1) %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+
