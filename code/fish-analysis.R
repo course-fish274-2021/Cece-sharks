b5836c40fa1c800ba059fb045cc36905a9f7b107
Cece <93742814+cwendel23@users.noreply.github.com>
2021-11-16 09:59:31 -0800

    Change size cutoff for new column

--- code/fish-analysis.R
+++ code/fish-analysis.R
@@ -1,9 +1,8 @@
-# Loading in the data 
+# Loading in the data & needed packages
 fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
-
-
 library(dplyr)
+library(ggplot2)
 
 # Adding column to denote fish is big or small 
 fish_data_cat <- fish_data %>%
-  mutate(length_cat = ifelse(length > 200, "big", "small"))
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
