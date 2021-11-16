84cb6554f768019f13af470e664971c1567c49d9
Cece <93742814+cwendel23@users.noreply.github.com>
2021-11-16 10:02:29 -0800

    Made comment more specific above fish_data_cat

--- code/fish-analysis.R
+++ code/fish-analysis.R
@@ -3,6 +3,6 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 library(dplyr)
 library(ggplot2)
 
-# Adding column to denote fish is big or small 
+# Adding column to denote if fish is big or small, using 300 as a cutoff
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
