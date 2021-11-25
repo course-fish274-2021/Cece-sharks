b144c5b9f37391e878afb9ce6f8b7d1108208b6e
Cece <93742814+cwendel23@users.noreply.github.com>
2021-11-24 17:32:56 -0800

    Added needed packages to shark script

---
+++ AustralianSharkIncidentDatabase-main/scripts/Cece-sharks-script.R
@@ -0,0 +1,39 @@
+# CODE FOR CECE-SHARKS PROJECT
+# BASED ON DATA FROM AUSTRALIAN SHARK INCIDENT DATABASE
+
+# R-packages required (copied from original database README file)
+  # *** Uncomment following install.packages lines if you need to install ***
+# install.packages("ggplot2")
+# install.packages("dplyr")
+# install.packages("forcats")
+# install.packages("leaflet")
+# install.packages("viridis")
+# install.packages("scales")
+# install.packages("ggmap")
+# install.packages("gridExtra")
+# install.packages("rgdal")
+# install.packages("magrittr")
+# install.packages("tibble")
+# install.packages("ozmaps")
+# install.packages("ggpubr")
+
+library(ggplot2)
+library(dplyr)
+library(forcats)
+library(leaflet)
+library(viridis)
+library(scales)
+library(ggmap)
+library(gridExtra)
+library(rgdal)
+library(magrittr)
+library(tibble)
+library(ozmaps)
+library(ggpubr)
+
+
+
+
+
+
+
